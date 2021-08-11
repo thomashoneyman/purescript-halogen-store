4647701b350b0f1b1fbc88ef6c3672206903d51c
Thomas Honeyman <admin@thomashoneyman.com>
2021-08-11 09:57:41 -0400

    Add purs-tidy

---
+++ shell.nix
@@ -0,0 +1,26 @@
+let
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/21.05.tar.gz";
+  }) {};
+
+  # To update to a newer version of easy-purescript-nix, run:
+  # nix-prefetch-git https://github.com/justinwoo/easy-purescript-nix
+  #
+  # Then, copy the resulting rev and sha256 here.
+  pursPkgs = import (pkgs.fetchFromGitHub {
+    owner = "justinwoo";
+    repo = "easy-purescript-nix";
+    rev = "eb64583e3e15749b3ae56573b2aebbaa9cbab4eb";
+    sha256 = "0hr7smk7avdgc5nm1r3drq91j1hf8wimp7sg747832345c8vq19a";
+  }) { inherit pkgs; };
+
+in pkgs.stdenv.mkDerivation {
+  name = "halogen-store";
+  buildInputs = with pursPkgs; [
+    pursPkgs.purs
+    pursPkgs.spago
+    pursPkgs.psa
+    pursPkgs.purs-tidy
+    pkgs.nodejs-14_x
+  ];
+}
